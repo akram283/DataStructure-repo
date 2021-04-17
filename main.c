adad905484f17e95a17879168df13639be33a004
akram283 <bidokimo5@gmail.com>
2021-04-17 17:11:22 +0200

    The first commit

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct student{
+    int ID;
+    int day,month,year;
+    int score;
+    char name[50];
+};
+typedef struct student st;
+int main()
+{
+
+    return 0;
+}
